b4e1760e72fa98eea987cb2f8f073ebe3a54b479
Joseph Jackson <96963864+jwseph@users.noreply.github.com>
2025-06-09 12:58:04 -0700

    edge runtime changes for cloudflare pages

--- app/api/scrape-books/route.ts
+++ app/api/scrape-books/route.ts
@@ -1,3 +1,5 @@
+export const runtime = 'edge';
+
 import { NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
--- app/results/[id]/page.tsx
+++ app/results/[id]/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = 'edge';
+
 "use client";
 
 import { useEffect, useState, useRef } from 'react';
